4da203d334f17a13ba87d5e802c9a65e99434245
Kirill Maltsev <opp.2211@gmail.com>
2021-01-21 21:04:26 +0500

    Context + MySql

---
+++ Store/Store/Models/WatchContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Store.Models
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class WatchContext : DbContext
+    {
+        public WatchContext() : base("conn") { }
+        public DbSet<Watch> Watches { get; set; }
+    }
+}
