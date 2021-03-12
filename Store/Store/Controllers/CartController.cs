f25634366d5e06ad2c483b13a923605f547392be
Kirill Maltsev <opp.2211@gmail.com>
2021-03-12 23:05:30 +0500

    CartController

---
+++ Store/Store/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using Store.Models;
+using Store.Comparers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text.Json;
+
+namespace Store.Controllers
+{
+    public class CartController : Controller
+    {
+        WatchContext db = new WatchContext();
+        public ActionResult Index()
+        {
+            WatchOrdList listWO = new WatchOrdList();
+            if (Session["goods"] != null)
+            {
+                listWO = JsonSerializer.Deserialize<WatchOrdList>(Session["goods"].ToString());
+            }
+
+            return View(listWO);
+        }
+        public ActionResult UpdateQuantt(int id, int quantt)
+        {
+            WatchOrdList listWO = new WatchOrdList();
+            if (Session["goods"] != null)
+            {
+                listWO = JsonSerializer.Deserialize<WatchOrdList>(Session["goods"].ToString());
+            }
+            WatchOrder WO = listWO.GetWObyID(id);
+
+            WO.Quantity += quantt;
+            if (WO.Quantity == 0)
+                listWO.Remove(WO);
+                
+
+            Session["goods"] = JsonSerializer.Serialize(listWO);
+            return View("Index", listWO);
+        }
+    }
+}
